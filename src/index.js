8459839bc58fb2762415138d6bb132b385867a86
oliver.bannister <oliver.bannister@roxorgaming.co.uk>
2020-01-09 19:05:48 +0000

    All requirements hit bar particles and scaling.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import * as PIXI from 'pixi.js';
 import * as eventEmitter from 'events';
 import gsap from 'gsap';
+import { TweenMax } from 'gsap';
 
 var app;
 var eventController;
 
 let gameContainer;
+let listOfPrizes = [1, 2, 3, 5, 10, 20, 100, 1000];
 
 const resources = [
     'resources/win_up_to_1k.png',
     'resources/play_button.png',
     'resources/play_button_active.png',
     'resources/game_panel_01.png',
-    'resources/coin_spin/coin_spin.json'
+    'resources/coin_spin/coin_spin.json',
+    'resources/prize_1.png',
+    'resources/prize_2.png',
+    'resources/prize_3.png',
+    'resources/prize_5.png',
+    'resources/prize_10.png',
+    'resources/prize_20.png',
+    'resources/prize_100.png',
+    'resources/prize_1000.png',
+    'resources/win_txt.png'
 ];
 
 window.onload = function () {
@@ -39,6 +50,13 @@ window.onload = function () {
     eventController.once('coinClicked', () => {
         animateCoin(23);
     });
+    eventController.once('coinDisappeared', () => {
+        const randomWinAmount = getRandomWinAmount();
+        createWinAmount(randomWinAmount);
+        if (randomWinAmount >= 100) {
+            createCongratulationsMessage(randomWinAmount);     
+        }
+    });
 
 
     function animate() {
@@ -58,7 +76,7 @@ const createHomeScreen = () => {
     winUpToTextureSprite.anchor.set(0.5);
     winUpToTextureSprite.x = app.renderer.screen.width / 2;
     winUpToTextureSprite.y = 100;
-    gsap.fromTo(winUpToTextureSprite, 1, { alpha: 0 }, { alpha: 1 });
+    TweenMax.fromTo(winUpToTextureSprite, 1, { alpha: 0 }, { alpha: 1 });
 
     const playTexture = app.loader.resources['resources/play_button.png'].texture;
     const playSprite = new PIXI.Sprite(playTexture);
@@ -75,7 +93,7 @@ const createHomeScreen = () => {
     playSprite.anchor.set(0.5);
     playSprite.x = app.renderer.screen.width / 2;
     playSprite.y = 300;
-    gsap.fromTo(playSprite, 1, { alpha: 0 }, { alpha: 1 });
+    TweenMax.fromTo(playSprite, 1, { alpha: 0 }, { alpha: 1 });
 
     gameContainer.addChild(winUpToTextureSprite);
     gameContainer.addChild(playSprite);
@@ -91,7 +109,7 @@ const createBonusPage = () => {
     sprite.y = 300;
     gameContainer.addChild(sprite);
     createStaticCoin();
-    gsap.fromTo(gameContainer, 1, { alpha: 0 }, { alpha: 1 });
+    TweenMax.fromTo(gameContainer, 1, { alpha: 0 }, { alpha: 1 });
     app.stage.addChild(gameContainer);
 };
 
@@ -110,6 +128,31 @@ const createStaticCoin = () => {
     gameContainer.addChild(sprite);
 };
 
+const getRandomWinAmount = () => listOfPrizes[Math.round((listOfPrizes.length * Math.random()) - 1)];
+
+
+const createWinAmount = (winAmount) => {
+    const texture = app.loader.resources[`resources/prize_${winAmount}.png`].texture;
+    const sprite = new PIXI.Sprite(texture);
+    sprite.x = app.renderer.screen.width / 2;
+    sprite.y = app.renderer.screen.height / 2;
+    sprite.anchor.set(0.5);
+    sprite.tint = Math.random() * 0xFFFFFF;
+    TweenMax.fromTo(sprite, 1, { alpha: 0 }, { alpha: 1 }); 
+    gameContainer.addChild(sprite);
+}
+
+const createCongratulationsMessage = () => {
+    const texture = app.loader.resources['resources/win_txt.png'].texture;
+    const sprite = new PIXI.Sprite(texture);
+    sprite.x = app.renderer.screen.width / 2;
+    sprite.y = 100;
+    sprite.scale.set(0.4);
+    sprite.anchor.set(0.5);
+    TweenMax.fromTo(sprite, 1, { alpha: 0 }, { alpha: 1 }); 
+    gameContainer.addChild(sprite);
+}
+
 const animateCoin = (numberOfFrames) => {
     const animatedCoinTextures = [];
     for (let i = 0; i < numberOfFrames; i++){
@@ -122,13 +165,17 @@ const animateCoin = (numberOfFrames) => {
     animatedCoin.y = app.renderer.screen.height / 2;
     animatedCoin.anchor.set(0.5);
     animatedCoin.gotoAndPlay(0);
-    // gsap.fromTo(animatedCoin, 1, { scale: 1 }, { scale: 0 }).delay(2);
+    TweenMax.fromTo(animatedCoin, 3, { alpha: 1 }, {
+        alpha: 0, onComplete: () => {
+            eventController.emit('coinDisappeared');
+        }
+    });
     gameContainer.addChild(animatedCoin);
 };
 
 const cleanGameContainer = () => {
     return new Promise((resolve) => {
-        gsap.fromTo(gameContainer, 1, { alpha: 1 }, { alpha: 0, onComplete: () => { 
+        TweenMax.fromTo(gameContainer, 1, { alpha: 1 }, { alpha: 0, onComplete: () => { 
             app.stage.removeChild(gameContainer);
             gameContainer.destroy();
             resolve();
